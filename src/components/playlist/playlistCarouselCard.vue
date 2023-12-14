396512de9e58390a0329998a2ea289c5aa8d458c
sleeping-slopes <weareinwin.rar@gmail.com>
2023-12-15 01:30:42 +0300

    moved playlist components

---
+++ src/components/playlist/playlistCarouselCard.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class = "playlist card">
+        <div class="cover-wrapper s128x128" v-on:click="$router.push($route.path+'/playlist/'+this.id)">
+            <img class = "cover" v-if="imageAvailable" :src="`http://localhost:5000/api/playlists/`+this.id+`/cover`" @error="imageAvailable=false"/>
+            <div class = "cover bi bi-music-note-list" v-else/>
+            <div class = "shade"></div>
+            <div class="playlist-hover playlist-stats h5">
+                <span class="icon-text">
+                    <span class="bi bi-music-note-list"></span><span>{{abbreviateNumber(this.playlist.songs_count)}}</span>
+                </span>
+                <span class="icon-text">
+                    <span class="bi bi-suit-heart-fill"></span><span>{{abbreviateNumber(this.playlist.likes_count)}}</span>
+                </span>
+            </div>
+            <button class="playlist-hover round-button large bi bi-play-fill"></button>
+        </div>
+        <div class= "song-info-wrapper">
+            <div class ="song-info h5">
+                <div v-for="(artist,index) in this.playlist.artists">
+                    <router-link class="artistlink secondary-text" v-if="artist.login"
+                        :to="'/id/'+artist.login"
+                        @click.stop>
+                        {{artist.name}}
+                    </router-link>
+                    <span class="secondary-text" v-else>{{artist.name}}</span>
+                    <span class="secondary-text" v-if="index+1 < this.playlist.artists.length">, </span>
+                </div>
+            </div>
+            <span class ="song-info primary-text h4" v-on:click="$router.push($route.path+'/playlist/'+this.id)">{{this.playlist.name}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import playlistCard from "@/components/playlist/playlistCard.vue"
+
+export default
+{
+  name: 'playlistCarouselCard',
+  extends: playlistCard
+}
+</script>
+
+<style>
+
+</style>
