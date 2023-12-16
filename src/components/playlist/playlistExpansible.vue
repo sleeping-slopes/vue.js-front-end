b1c6b487414133fa141a2c4f300870f95d8de410
sleeping-slopes <weareinwin.rar@gmail.com>
2023-12-16 03:12:07 +0300

    created playlistExpansible component

---
+++ src/components/playlist/playlistExpansible.vue
@@ -0,0 +1,134 @@
+<template>
+    <div class = "playlist-expansible" v-bind:class="{'expanded': this.expanded}">
+        <div style="display:flex;flex-direction: column;">
+            <div class="left-column">
+                <div class="cover-wrapper s160x160" v-on:click="$router.push($route.path+'/playlist/'+this.id)">
+                    <img class = "cover" v-if="imageAvailable" :src="`http://localhost:5000/api/playlists/`+this.id+`/cover`" @error="imageAvailable=false"/>
+                    <div class = "cover bi bi-music-note-list" v-else/>
+                </div>
+                <div>
+                    <button class="button-secondary h5" style="margin:auto" v-bind:class="{'toggled': this.playlist.liked}" v-on:click.stop="this.like()">
+                        <div class="icon-text">
+                            <span class="bi bi-suit-heart-fill"></span><span>Like{{ this.playlist.liked?'d':'' }}</span>
+                        </div>
+                    </button>
+                </div>
+            </div>
+            <div class="empty-column-panel">
+                <div class="empty-column-main"></div>
+            </div>
+        </div>
+        <div class="right-column">
+            <div style="display:flex;flex-direction: row; gap:10px; align-items: center;">
+                <button class="round-button medium" v-bind:class="this.isPlaying?'bi bi-pause-circle-fill':'bi bi-play-circle-fill'" v-on:click.stop="playPlaylist()"></button>
+                <div class= "song-info-wrapper">
+                    <div class ="song-info h4">
+                        <div v-for="(artist,index) in this.playlist.artists">
+                            <router-link class="artistlink secondary-text" v-if="artist.login"
+                                :to="'/id/'+artist.login"
+                                @click.stop>
+                                {{artist.name}}
+                            </router-link>
+                            <span class="secondary-text" v-else>{{artist.name}}</span>
+                            <span class="secondary-text" v-if="index+1 < this.playlist.artists.length">, </span>
+                        </div>
+                    </div>
+                    <span class ="song-info primary-text h3" v-on:click="$router.push($route.path+'/playlist/'+this.id)">{{this.playlist.name}}</span>
+                </div>
+            </div>
+            <hr>
+            <playlist class="ul-list" style="overflow-y:auto"
+            :id="this.id"
+            :songs="this.songs"
+            :dynamicComponent="'song'"/>
+            <div v-if="this.songs.length>4">
+                <button class="button-secondary h6" style="margin:auto" v-on:click="this.expanded=!this.expanded">{{this.expanded?"Hide":"Expand"}}</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import playlistCard from "@/components/playlist/playlistCard.vue"
+import playlist from "@/components/playlist.vue"
+import { getPlaylistSongs } from "@/axios/getters"
+
+export default
+{
+  name: 'playlistExpansible',
+  components: {playlist},
+  extends: playlistCard,
+  data()
+  {
+    return {
+        expanded:false
+    }
+  },
+  async mounted()
+  {
+    this.songs = await getPlaylistSongs(this.id);
+  }
+}
+</script>
+
+<style>
+
+.playlist-expansible
+{
+    display:flex;
+    max-height:288px;
+    flex-direction: row;
+    height:fit-content;
+    flex-shrink: 0;
+    transition: 0.5s all;
+}
+
+.playlist-expansible.expanded
+{
+    max-height:624px;
+}
+
+.left-column
+{
+    display:flex;
+    flex-direction: column;
+    height:min-content;
+    padding:5px;
+    box-sizing: border-box;
+    gap:5px;
+    border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px;
+    background-color: var(--panel-background-color);
+}
+
+.right-column
+{
+    display:flex;
+    flex-direction: column;
+    width:100%;
+    gap:5px;
+    padding:5px;
+    box-sizing: border-box;
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px;
+    border-bottom-left-radius: 10px;
+    background-color: var(--panel-background-color);
+    min-height:253px;
+}
+
+.empty-column-panel
+{
+    height:100%;
+    background-color: var(--panel-background-color);
+}
+
+.empty-column-main
+{
+    height:100%;
+    background-color: var(--main-background-color);
+    border-top-right-radius: 10px;
+}
+
+
+</style>
