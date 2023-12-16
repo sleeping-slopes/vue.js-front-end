fb2b95a9f164a4da22782bdcc084dd0fd193f4ca
sleeping-slopes <weareinwin.rar@gmail.com>
2023-12-16 03:14:50 +0300

    added all/created/liked select

--- src/views/YourPlaylistsView.vue
+++ src/views/YourPlaylistsView.vue
@@ -1,26 +1,39 @@
 <template>
   <div class="column">
-    <div style="display:flex; align-items: center;" v-if="this.playlists.length>0">
+    <div style="display:flex; align-items: center;">
       <span class="h3 secondary-text">Hear your own playlists and the playlists you’ve liked:</span>
       <div style="display:flex; margin-left:auto; align-items: center; gap:20px;">
+        <div style="display:flex; margin-left:auto; align-items: center; gap:5px;">
+          <span class="secondary-text h4">View</span>
+          <button v-bind:class="{'toggled': this.currentViewStyle==1}" @click="this.currentViewStyle=1" class="button-secondary bi bi-grid-fill h3" style="padding:5px;"></button>
+          <button v-bind:class="{'toggled': this.currentViewStyle==0}" @click="this.currentViewStyle=0" class="button-secondary bi bi-list h3" style="padding:5px;"></button>
+        </div>
         <div class="wrapper-search h4">
           <div class="search-panel">
             <input class="search" type="text" placeholder="Filter" autocomplete="off">
             <i class="fa fa-search"></i>
           </div>
         </div>
-        <button class="button-primary h5">Dropdown</button>
+        <select v-model="selected">
+          <option>All</option>
+          <option>Created</option>
+          <option>Liked</option>
+        </select>
       </div>
     </div>
-    <errorMessage  v-else>
+    <errorMessage  v-if="this.playlists.length==0">
       <template v-slot:errorIcon><span class="bi bi-music-note-list"></span></template>
       <template v-slot:message>No playlists here yet</template>
     </errorMessage>
-    <ul class="ul-grid">
+    <ul class="column" v-if="this.currentViewStyle==0">
       <li v-for="playlist in this.playlists">
-        <playlistCard :id="playlist.id"/>
+        <playlistExpansible :id="playlist.id" :key="playlist.id"/>
       </li>
-
+    </ul>
+    <ul class="ul-grid" v-else>
+      <li v-for="playlist in this.playlists">
+        <playlistCard :id="playlist.id" :key="playlist.id"/>
+      </li>
     </ul>
   </div>
   <router-view></router-view>
@@ -29,25 +42,44 @@
   <script>
 
   import playlistCard from '@/components/playlist/playlistCard.vue'
+  import playlistExpansible from '@/components/playlist/playlistExpansible.vue'
   import errorMessage from '@/components/errorMessage.vue'
 
-  import {getPlaylists} from "@/axios/getters.js"
+  import {getUserAllPlaylists, getUserCreatedPlaylists, getUserLikedPlaylists} from "@/axios/getters.js"
 
   export default {
     name: 'YourPlaylistsView',
     components:
     {
-      playlistCard,errorMessage
+      playlistCard,playlistExpansible,errorMessage
     },
     data()
     {
       return {
         playlists:[],
+        selected:'All',
+        currentViewStyle:1,
       }
     },
-    async mounted()
+    props:
     {
-      this.playlists = await getPlaylists();
+      login:{default:"nologin"}
+    },
+    watch:
+    {
+      'selected':
+      {
+        handler: async function(value)
+        {
+          switch (value)
+          {
+            case "All": this.playlists = await getUserAllPlaylists(this.login); break;
+            case "Created": this.playlists = await getUserCreatedPlaylists(this.login); break;
+            case "Liked": this.playlists = await getUserLikedPlaylists(this.login); break;
+          }
+        },
+        immediate: true
+      }
     }
   }
   </script>
--- src/views/YourSongsView.vue
+++ src/views/YourSongsView.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="column">
-    <div style="display:flex; align-items: center;" v-if="this.playlist.songs.length>0">
+    <div style="display:flex; align-items: center;">
       <span class="h3 secondary-text">Hear your own songs and the songs you’ve liked:</span>
       <div style="display:flex; margin-left:auto; align-items: center; gap:20px;">
         <div style="display:flex; margin-left:auto; align-items: center; gap:5px;">
           <span class="secondary-text h4">View</span>
-          <button v-bind:class="{'toggled': this.currentViewStyle==1}" @click="this.currentViewStyle=1" class="button-secondary bi bi-grid-fill h3" style="height:auto;padding:5px;"></button>
-          <button v-bind:class="{'toggled': this.currentViewStyle==0}" @click="this.currentViewStyle=0" class="button-secondary bi bi-list h3" style="height:auto;padding:5px;"></button>
+          <button v-bind:class="{'toggled': this.currentViewStyle==1}" @click="this.currentViewStyle=1" class="button-secondary bi bi-grid-fill h3" style="padding:5px;"></button>
+          <button v-bind:class="{'toggled': this.currentViewStyle==0}" @click="this.currentViewStyle=0" class="button-secondary bi bi-list h3" style="padding:5px;"></button>
         </div>
         <div class="wrapper-search h4">
           <div class="search-panel">
@@ -14,18 +14,22 @@
             <i class="fa fa-search"></i>
           </div>
         </div>
-        <button class="button-primary h5">Dropdown</button>
+        <select v-model="selected">
+          <option>All</option>
+          <option>Created</option>
+          <option>Liked</option>
+        </select>
       </div>
     </div>
     <panel v-if="this.currentViewStyle==0">
       <template v-slot:content>
-        <playlist :class="'ul-list'"
+        <playlist :class="'ul-list hidden-scroll'"
           :id="playlist.id"
           :songs="playlist.songs"
           :dynamicComponent="'songExtended'"/>
       </template>
     </panel>
-    <playlist :class="'ul-grid'" v-else
+    <playlist :class="'ul-grid hidden-scroll'" v-else
       :id="playlist.id"
       :songs="playlist.songs"
       :dynamicComponent="'songCard'"/>
@@ -38,7 +42,7 @@
   import playlist from '@/components/playlist.vue';
   import songCard from '@/components/song/songCard.vue'
 
-  import {getSongs} from "@/axios/getters.js"
+  import { getUserAllSongs, getUserCreatedSongs, getUserLikedSongs} from "@/axios/getters.js"
 
   export default {
     name: 'YourSongsView',
@@ -55,11 +59,28 @@
           songs:[],
         },
         currentViewStyle:1,
+        selected:'All',
       }
     },
-    async mounted()
+    props:
     {
-      this.playlist.songs = await getSongs();
+      login:{default:"nologin"}
+    },
+    watch:
+    {
+      'selected':
+      {
+        handler: async function(value)
+        {
+          switch (value)
+          {
+            case "All": this.playlist.id="[]YouAll";this.playlist.songs = await getUserAllSongs(this.login); break;
+            case "Created": this.playlist.id="[]YouCreated";this.playlist.songs = await getUserCreatedSongs(this.login); break;
+            case "Liked": this.playlist.id="[]YouLiked";this.playlist.songs = await getUserLikedSongs(this.login); break;
+          }
+        },
+        immediate: true
+      }
     }
   }
   </script>
