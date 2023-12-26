f0a43cc81b6d0940ec008576555c78c2bd80f210
sleeping-slopes <weareinwin.rar@gmail.com>
2023-12-27 02:17:21 +0300

    Create FeedView.vue

---
+++ src/views/FeedView.vue
@@ -0,0 +1,17 @@
+<template>
+    <panel style="height:100%;">
+        <template v-slot:header>Songs</template>
+        <template v-slot:content>TEST</template>
+    </panel>
+</template>
+
+<script>
+
+import panel from '@/components/containers/panel.vue';
+
+export default
+{
+name: 'FeedView',
+components: { panel }
+}
+</script>
