c4e835a833139ad96a420eb1b946897781c4020f
sleeping-slopes <weareinwin.rar@gmail.com>
2024-01-02 01:12:02 +0300

    Update FeedView.vue

--- src/views/FeedView.vue
+++ src/views/FeedView.vue
@@ -5,52 +5,50 @@
     <template v-slot:message>{{ this.song.error.message }}</template>
 </errorMessage>
 <template v-else>
-    <songLarge :id="this.id" />
-    <div class="content">
-        <div class="column">
-            <panel>
-                    <template v-slot:header>Related</template>
-                    <template v-slot:menu>
-                        <button class="button-secondary h5">
-                            View all
-                        </button>
-                    </template>
-                    <template v-slot:content>
-                        <songContainer class="ul-list hidden-scroll"
-                        :playlist="{id:'zalupa',songs:[{id:1,pos:0},{id:1,pos:1},{id:1,pos:2}]}"
-                        :dynamicComponent="'songExtended'"
-                        />
-                    </template>
-            </panel>
-            <panel>
-                    <template v-slot:header>Playlists</template>
-                    <template v-slot:menu>
-                        <button class="button-secondary h5">
-                            View all
-                        </button>
-                    </template>
-                    <template v-slot:content>
-                        <songContainer class="ul-list hidden-scroll"
-                        :playlist="{id:'zalupa',songs:[{id:1,pos:0},{id:1,pos:1},{id:1,pos:2}]}"
-                        :dynamicComponent="'songExtended'"
-                        />
-                    </template>
-            </panel>
-            <panel>
-                    <template v-slot:header>Likes</template>
-                    <template v-slot:menu>
-                        <button class="button-secondary h5">
-                            View all
-                        </button>
-                    </template>
-                    <template v-slot:content>
-                        <songContainer class="ul-list hidden-scroll"
-                        :playlist="{id:'zalupa',songs:[{id:1,pos:0},{id:1,pos:1},{id:1,pos:2}]}"
-                        :dynamicComponent="'songExtended'"
-                        />
-                    </template>
-            </panel>
-        </div>
+    <songBanner :id="this.id" />
+    <div class="content row">
+        <panel style="width:100%">
+                <template v-slot:header>Related songs</template>
+                <template v-slot:menu>
+                    <button class="button-secondary h5">
+                        View all
+                    </button>
+                </template>
+                <template v-slot:content>
+                    <songContainer class="ul-list hidden-scroll"
+                    :playlist="{id:'zalupa',songs:[{id:1,pos:0},{id:1,pos:1},{id:1,pos:2}]}"
+                    :dynamicComponent="'songExtended'"
+                    />
+                </template>
+        </panel>
+        <panel style="width:50%">
+                <template v-slot:header>In playlists</template>
+                <template v-slot:menu>
+                    <button class="button-secondary h5">
+                        View all
+                    </button>
+                </template>
+                <template v-slot:content>
+                    <songContainer class="ul-list hidden-scroll"
+                    :playlist="{id:'zalupa',songs:[{id:1,pos:0},{id:1,pos:1},{id:1,pos:2}]}"
+                    :dynamicComponent="'songExtended'"
+                    />
+                </template>
+        </panel>
+        <panel style="width:50%">
+                <template v-slot:header>Likes</template>
+                <template v-slot:menu>
+                    <button class="button-secondary h5">
+                        View all
+                    </button>
+                </template>
+                <template v-slot:content>
+                    <songContainer class="ul-list hidden-scroll"
+                    :playlist="{id:'zalupa',songs:[{id:1,pos:0},{id:1,pos:1},{id:1,pos:2}]}"
+                    :dynamicComponent="'songExtended'"
+                    />
+                </template>
+        </panel>
     </div>
 </template>
 </template>
@@ -58,15 +56,14 @@
 <script>
 
 import panel from '@/components/containers/panel.vue';
-import API from "@/axios/API";
-import songLarge from "@/components/songs/songLarge.vue";
+import songBanner from "@/components/songs/songBanner.vue";
 import errorMessage from "@/components/containers/errorMessage.vue";
 import songContainer from '@/components/songContainer.vue';
 
 export default
 {
     name: 'FeedView',
-    components:{panel, songLarge, errorMessage, songContainer},
+    components:{panel, songBanner, errorMessage, songContainer},
     props:
     {
         id: { default: "12" },
@@ -76,10 +73,6 @@ export default
         return {
             song:{},
         }
-    },
-    async created()
-    {
-        this.songs = await API.get('songs/'+this.id);
     }
 }
 </script>
