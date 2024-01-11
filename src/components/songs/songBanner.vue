9f9fc63e59d056dfa7039d8c056aee32fd42a82b
sleeping-slopes <weareinwin.rar@gmail.com>
2024-01-11 09:53:21 +0300

    add colorthief

--- src/components/songs/songBanner.vue
+++ src/components/songs/songBanner.vue
@@ -1,18 +1,23 @@
 <template>
-    <div class="banner sticky-top dark">
-        <div class="banner-bg gradient-bg"/>
+    <div class="row gap-20" style="overflow: hidden; width:1200px;">
+        <div class="banner-bg" :style="this.gradient" />
         <div class="column w-100" style="overflow:hidden;">
             <div class="row">
                 <button class="button button-round huge toggled" v-bind:class="this.isPlaying?'bi bi-pause-circle-fill':'bi bi-play-circle-fill'" v-on:click="setCurrentSong"></button>
                 <div class="info-wrapper column gap-10">
-                    <h3 class="banner-info-secondary">
+                    <h3 class="banner-info banner-info-secondary">
                         <template v-for="(artist,index) in this.song.artists">
                             <router-link :to="{ name: 'User', params: { login: artist.login }}" class="artistlink" v-if="artist.login">{{artist.name}}</router-link>
                             <span v-else>{{artist.name}}</span>
                             <span v-if="index+1 < this.song.artists.length">, </span>
                         </template>
                     </h3>
-                    <h2 class="banner-info-primary">{{ this.song.name }}</h2>
+                    <h2 class="banner-info banner-info-primary">{{ this.song.name }}</h2>
+                    <span class="h5 banner-info banner-info-secondary">
+                    {{ abbreviateNumber(53252) }} play{{ 53252!=1?'s':'' }}
+                    <span class="bi bi-dot"></span>
+                    {{ abbreviateNumber(this.song.likes_count) }} like{{ this.song.likes_count!=1?'s':'' }}
+                    </span>
                 </div>
                 <ul class="tag-container right">
                     <li v-for="tag in this.song.tags"><router-link class="button-tag h6" :to="{ name: 'Tag', params: { tag: tag.tag } }">{{tag.tag}}</router-link></li>
@@ -20,42 +25,78 @@
             </div>
 
             <div class="row bottom gap-5 y-center">
-                <button class="button button-secondary h5 icon-text">
+                <button class="button button-banner h5 icon-text" style="width:75px;">
                     <span class="bi bi-suit-heart-fill"></span><span>Like</span>
                 </button>
-                <button class="button button-secondary h5 icon-text">
+                <button class="button button-banner h5 icon-text" style="width:135px;">
                     <span class="bi bi-music-note-list"></span><span>Add to playlist</span>
                 </button>
-                <button class="button button-secondary h5 icon-text">
+                <button class="button button-banner h5 icon-text" style="width:100px;">
                     <span class="bi bi-music-note-list"></span><span>Play next</span>
                 </button>
-                <button class="button button-secondary h5 icon-text">
+                <button class="button button-banner h5 icon-text" style="width:100px;">
                     <span class="bi bi-link"></span><span>Copy link</span>
                 </button>
-                <button class="button button-secondary h5 icon-text">
+                <button class="button button-banner h5 icon-text" style="width:105px;">
                     <span class="bi bi-download"></span><span>Download</span>
                 </button>
-                <span class="h5 secondary-text right">
-                    {{ abbreviateNumber(53252) }} play{{ 53252!=1?'s':'' }}
-                    <span class="bi bi-dot"></span>
-                    {{ abbreviateNumber(this.song.likes_count) }} like{{ this.song.likes_count!=1?'s':'' }}
-                </span>
+
             </div>
         </div>
         <div class = "cover-wrapper s320x320 right">
-            <img class = "cover" v-if="imageAvailable" :src="coversrc" @error="imageAvailable=false" />
+            <img id="songCover" class = "cover" v-if="imageAvailable" :src="coversrc" @error="imageAvailable=false" />
             <div class = "cover bi bi-music-note" v-else/>
         </div>
     </div>
-  </template>
+</template>
+
 <script>
 
 import song from "@/components/songs/song.vue"
+import ColorThief from 'colorthief/dist/color-thief.mjs'
 
 export default
 {
   name: 'songBanner',
-  extends: song
+  extends: song,
+  data()
+  {
+    return {
+        colorA:'var(--panel-border-color)',
+        colorB:'var(--main-background-color)'
+    }
+  },
+  computed:
+  {
+    gradient()
+    {
+        return "background: linear-gradient(135deg, "+this.colorA+" 0%, "+this.colorB+" 100%)";
+    }
+  },
+  methods:
+  {
+    setColors()
+    {
+        const colorThief = new ColorThief();
+        const img = document.getElementById('songCover');
+        const colors = colorThief.getPalette(img,2).map(color=>{ return "rgb("+color.join(',')+")"});
+        [this.colorA,this.colorB] = colors;
+    }
+  },
+  mounted()
+  {
+
+    const img = document.getElementById('songCover');
+    img.crossOrigin = 'Anonymous';
+    if (img.complete)
+    {
+        this.setColors();
+    }
+    else
+    {
+        img.addEventListener('load', this.setColors);
+    }
+  }
 }
 
 </script>
