718a8c205b71f3304ed1cbc570d3d9db3c5b0b4e
sleeping-slopes <weareinwin.rar@gmail.com>
2024-01-21 19:39:46 +0300

    create user skeletons

---
+++ src/components/users/skeletons/userIcon Skeleton.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class = "user-image user-icon s36x36 shimmer"></div>
+</template>
+
+<script>
+
+import userSkeletonInterface from '@/components/users/skeletons/userSkeleton interface.vue';
+
+export default
+{
+    name: 'userIconSkeleton',
+    extends: userSkeletonInterface
+}
+
+</script>
+
+<style scoped>
+
+.user-icon
+{
+    border:2px solid var(--panel-background-color);
+    margin-right:-10px;
+    box-sizing: border-box;
+}
+
+</style>
---
+++ src/components/users/skeletons/userItem Skeleton.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class = "row">
+        <div class = "user-image s160x160 shimmer"></div>
+        <div class= "info-wrapper column" style="justify-content: center;">
+            <skeleton class ="h3 shimmer"></skeleton>
+            <skeleton class ="h5 shimmer"></skeleton>
+            <skeleton class="button button-secondary h2 shimmer" :exactwidth="90"/>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import userSkeletonInterface from '@/components/users/skeletons/userSkeleton interface.vue';
+
+export default
+{
+    name: 'userItemSkeleton',
+    extends: userSkeletonInterface
+}
+
+</script>
