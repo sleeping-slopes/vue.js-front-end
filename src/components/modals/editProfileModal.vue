9e1ed3a5f806ec2330a391914d837a3bd440213c
sleeping-slopes <weareinwin.rar@gmail.com>
2024-02-07 00:51:37 +0300

    add uploadImage

--- src/components/modals/editProfileModal.vue
+++ src/components/modals/editProfileModal.vue
@@ -16,8 +16,13 @@
                             </button>
                         </template>
                         <template v-slot:options>
-                            <button type = "button" class="button h6">Replace image</button>
-                            <button type = "button" class="button h6">Delete image</button>
+                            <li>
+                                <button type = "button" class="button h6" v-on:click="this.$refs.profilePictureInput.click()">Replace image</button>
+                                <input type="file" ref="profilePictureInput" style="display:none" v-on:change="uploadImage" accept="image/*" />
+                            </li>
+                            <li>
+                                <button type = "button" class="button h6">Delete image</button>
+                            </li>
                         </template>
                     </contextMenu>
                 </div>
@@ -111,8 +116,11 @@
 <script>
 
 import API from "@/axios/API";
+
 import modal from "@/components/containers/modal.vue";
 import contextMenu from "@/components/containers/contextMenu.vue";
+
+import { validURL } from "@/functions.js"
 
 export default
 {
@@ -202,7 +210,7 @@ export default
                         this.links[i].url.data = "http://"+this.links[i].url.data;
                     }
                 }
-                if (!this.validURL(this.links[i].url.data))this.links[i].url.error = "This URL or email is invalid.";
+                if (!validURL(this.links[i].url.data))this.links[i].url.error = "This URL or email is invalid.";
             }
             else this.links[i].url.error = "Enter a web or email address.";
 
@@ -234,10 +242,15 @@ export default
             this.$router.replace({query: null});
         }
     },
-    validURL(url)
+    async uploadImage(event)
     {
-      const regex = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
-      return regex.test(url);
+        const selectedFile = event.target.files[0];
+
+        if (!selectedFile) return;
+
+        const fd = new FormData();
+        fd.append('userProfilePicture',selectedFile,selectedFile.name);
+        const r = await API.post('me/profile-picture', fd);
     }
   }
 }
