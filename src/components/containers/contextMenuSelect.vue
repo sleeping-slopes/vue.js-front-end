bac8444e88c2271e162345ec10c39c09f78de44e
sleeping-slopes <weareinwin.rar@gmail.com>
2024-02-13 00:00:41 +0300

    create contextMenuSelect.vue

---
+++ src/components/containers/contextMenuSelect.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="context-menu-select" v-bind:class="{'context-menu-select-active': this.active}">
+        <div class="context-menu-select-header" ref="header" v-on:click="this.active=!this.active">
+            <slot name="header"></slot>
+        </div>
+        <ul class="context-menu-select-options">
+            <slot name="options"></slot>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+export default
+{
+    name: 'contextMenuSelect',
+    data()
+    {
+        return {
+            active:false
+        }
+    },
+    mounted()
+    {
+        document.addEventListener('click', this.close);
+    },
+    beforeUnmount()
+    {
+        document.removeEventListener('click', this.close);
+    },
+    methods:
+    {
+        close(e)
+        {
+            if (!this.$refs.header.contains(e.target)) this.active=false;
+        }
+    }
+}
+
+</script>
+
+<style>
+
+.context-menu-select
+{
+    position:relative;
+    border-radius:8px;
+}
+
+.context-menu-select .context-menu-select-header
+{
+    height:100%;
+    width:100%;
+}
+
+.context-menu-select .context-menu-select-options
+{
+    display:flex;
+    flex-direction: column;
+    width:100%;
+    position:absolute;
+    z-index:10;
+    box-shadow: 0 0 15px 1px rgba(0,0,0,.2);
+    visibility:hidden;
+}
+
+.context-menu-select.context-menu-select-active .context-menu-select-options { visibility:visible; }
+
+</style>
