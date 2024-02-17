d236edfb0351d3a21d6770ae07ea9fd239dfa3d5
sleeping-slopes <weareinwin.rar@gmail.com>
2024-02-18 00:30:15 +0300

    Update FeedView.vue

--- src/views/FeedView.vue
+++ src/views/FeedView.vue
@@ -1,33 +1,26 @@
 <template>
-<errorMessage>
-    <template v-slot:errorIcon><span class="bi bi-emoji-frown"></span></template>
-    <template v-slot:status>{{ "200" }}</template>
-    <template v-slot:message>{{ "WIP: Feed View" }}</template>
-</errorMessage>
-<div class="content column">
-    <div class="primary-text column">
-        <span class="font-size-large">Hello world! font-size-large</span>
-        <span class="font-size-big">Hello world! font-size-big</span>
-        <span class="font-size-medium">Hello world! font-size-medium</span>
-        <span class="font-size-small">Hello world! font-size-small</span>
-        <span class="font-size-tiny">Hello world! font-size-tiny</span>
+    <div class="content column">
         <h1 class="primary-text font-size-large" style="margin-top:30px">Feed</h1>
+        <errorMessage>
+            <template v-slot:errorIcon><span class="bi bi-emoji-frown"></span></template>
+            <template v-slot:status>{{ "200" }}</template>
+            <template v-slot:message>{{ "WIP: Feed View" }}</template>
+        </errorMessage>
+        <article class="project-info row">
+            <p>
+                <span>About website</span><br>
+                Non-commercial project created solely for demonstration purposes.<br>
+                I do not own the copyrights to the posted content.
+            </p>
+            <p>
+                <span>Technologies used</span><br>
+                Frontend: HTML, CSS (PostCSS), Vue.js, Axios;<br>
+                Backend: Express.js, MySQL, REST API, Multer, JSON Web Token.
+            </p>
+            <p>Created by <a href="https://github.com/sleeping-slopes" class="accent-text alink" target=”_blank” rel="noopener noreferrer">@sleeping-slopes</a></p>
+            <p class="right"><span class="accent-text">Language:&nbsp</span>English (US)</p>
+        </article>
     </div>
-    <article class="project-info row">
-        <p>
-            <span>About website</span><br>
-            Non-commercial project created solely for demonstration purposes.<br>
-            I do not own the copyrights to the posted content.
-        </p>
-        <p>
-            <span>Technologies used</span><br>
-            Frontend: HTML, CSS (PostCSS), Vue.js, Axios<br>
-            Backend: Express.js, MySQL, REST API, JSON Web Token
-        </p>
-        <p>Created by <a href="https://github.com/sleeping-slopes" class="accent-text alink" target=”_blank” rel="noopener noreferrer">@sleeping-slopes</a></p>
-        <p class="right"><span class="accent-text">Language:&nbsp</span>English (US)</p>
-    </article>
-</div>
 </template>
 
 <script>
