1477a86d2e415e1786d5f570b3e36f6404e62e21
sleeping-slopes <weareinwin.rar@gmail.com>
2024-02-23 00:00:58 +0300

    replace <input type="range> with slider.vue component

--- src/components/player.vue
+++ src/components/player.vue
@@ -2,10 +2,9 @@
   <audio ref="audio"
     @timeupdate="this.currentTime = $event.target.currentTime"
     @ended="this.$store.dispatch('playNextSong')"
-    :volume="volume"
+    :volume="this.$store.state.volume"
     preload="none">
   </audio>
-
   <div class="player" v-if="this.currentSongID!=null">
     <div class="player-menu">
       <button class="button button-default button-small bi bi-skip-start-fill"
@@ -28,18 +27,31 @@
     </div>
     <div class="player-slider">
       <span class="song-time font-size-small"> {{ numberToTimeString(this.currentTime) }}</span>
-      <input class="song-slider" ref="slider" type="range" min=0 :max="this.$refs.audio.duration" step=0.1 v-model="currentTime" @change="seek()">
+      <slider style="width:100%"
+        :min-value="0"
+        :value="currentTime"
+        :max-value="this.$refs.audio.duration || this.$store.getters.getSong(this.currentSongID).duration"
+        @changeProgress="setProgress">
+      </slider>
       <span class="song-time font-size-small"> {{ numberToTimeString(this.$refs.audio.duration || this.$store.getters.getSong(this.currentSongID).duration) }}</span>
     </div>
     <div class="row gap-20">
       <div class="popup-wrapper">
         <div class="player-menu">
-          <button class="button button-default button-small" v-bind:class="{ 'bi bi-volume-up-fill': volume>=0.5,'bi bi-volume-down-fill': volume>0 && volume<0.5,'bi bi-volume-mute-fill': volume==0,'toggled':this.showVolume }"
+          <button class="button button-default button-small" v-bind:class="{ 'bi bi-volume-up-fill': this.$store.state.volume>=0.5,'bi bi-volume-down-fill': this.$store.state.volume>0 && this.$store.state.volume<0.5,'bi bi-volume-mute-fill': this.$store.state.volume==0,'toggled':this.showVolume }"
             v-on:click="this.showVolume=!this.showVolume">
           </button>
         </div>
-        <panel class="popup volume-popup" v-bind:class="this.showVolume?'visible':'hidden'">
-          <template v-slot:content><input v-model="volume" type="range" min=0 max=1 step=0.01 orient="vertical"/></template>
+        <panel class="popup volume-popup" v-bind:class="this.showVolume?'visible':'hidden'" style="padding-bottom:10px;padding-top:10px;">
+          <template v-slot:content>
+            <slider style="height:100px;"
+              :vertical="true"
+              :min-value="0"
+              :value="this.$store.state.volume"
+              :max-value="1"
+              @changeProgress="setVolume">
+            </slider>
+          </template>
         </panel>
       </div>
       <div style="width:320px;position:relative;">
@@ -80,19 +92,19 @@
 
 <script>
 
+import API from "@/axios/API"
+
 import panel from "@/components/containers/panel.vue"
+import slider from "@/components/slider.vue"
 import songContainer from "@/components/containers/songContainer.vue"
 import songItem from "@/components/songs/songItem.vue"
-import songItemSkeleton from "@/components/songs/skeletons/songItem Skeleton.vue"
 
 import { numberToTimeString } from "@/functions.js"
-
-import API from "@/axios/API"
 
 export default
 {
   name: 'player',
-  components: { panel, songContainer, songItem, songItemSkeleton },
+  components: { panel, slider, songContainer, songItem },
   data()
   {
     return {
@@ -102,29 +114,14 @@ export default
       showVolume:false
     }
   },
-  async mounted()
-  {
-    this.currentSongID = this.$store.getters.getCurrentSong;
-    if (this.currentSongID!=null)
-    {
-
-      this.$refs.audio.src = API.defaults.baseURL+`songs/`+this.currentSongID+`/audio`;
-    }
-  },
   computed:
   {
     liked(){ return this.$store.getters.getSong(this.currentSongID).liked; },
-    volume:
-    {
-      get () { return this.$store.state.volume; },
-      set (value) { this.$store.commit('setVolume', value); }
-    }
   },
   watch:
   {
     '$store.getters.getCurrentPlaylistSongPos'(newPlaylistSong,oldPlaylistSong)
     {
-
       if (newPlaylistSong.edited && oldPlaylistSong && (newPlaylistSong.edited != oldPlaylistSong.edited) ) return;
       this.currentSongID = this.$store.getters.getCurrentSong;
       if (this.currentSongID!=null)
@@ -140,17 +137,29 @@ export default
   },
   methods:
   {
-    seek()
+    setProgress(value)
     {
-      this.$refs.audio.currentTime=this.$refs.slider.value;
+      this.$refs.audio.currentTime=value;
       this.$store.state.isPlaying=true;
+    },
+    setVolume(value)
+    {
+      this.$store.commit('setVolume', value);
     },
     likeCurrentSong()
     {
       this.$refs.song.like();
     },
     numberToTimeString:numberToTimeString
-  }
+  },
+  async mounted()
+  {
+    this.currentSongID = this.$store.getters.getCurrentSong;
+    if (this.currentSongID!=null)
+    {
+      this.$refs.audio.src = API.defaults.baseURL+`songs/`+this.currentSongID+`/audio`;
+    }
+  },
 }
 
 </script>
@@ -227,16 +236,13 @@ export default
   justify-content: center;
 }
 
-.song-slider
-{
-  min-width:250px;
-  width:100%;
-}
 
 .player-slider
 {
+  min-width:250px;
   display:flex;
   width:100%;
+  align-items:center;
 }
 
 </style>
